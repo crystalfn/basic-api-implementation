e8e4ac031a3d5c22925a263f6ee64ba7f822a84d
crystalfn <969274162@qq.com>
2020-09-15 19:41:55 +0800

    [fangni] feat: implement get rs event by range

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -15,4 +15,9 @@ public class RsController {
   public String getOneRsEvent(@PathVariable int index) {
     return rsList.get(index - 1);
   }
+
+  @GetMapping("rs/event")
+  public String getRsEventByRange(int start, int end) {
+    return rsList.subList(start - 1, end).toString();
+  }
 }
