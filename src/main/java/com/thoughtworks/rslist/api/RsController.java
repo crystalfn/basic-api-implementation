8312b905ae740b85dde3e0aee20669ce6aec8e8d
crystalfn <969274162@qq.com>
2020-09-15 20:21:11 +0800

    [fangni] feat: implement add one rs event

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -2,27 +2,42 @@ package com.thoughtworks.rslist.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+    private List<String> rsList = initRsList();
 
-  @GetMapping("/rs/{index}")
-  public String getOneRsEvent(@PathVariable int index) {
-    return rsList.get(index - 1);
-  }
-
-  @GetMapping("rs/list")
-  public String getRsEventByRange(@RequestParam(required = false) Integer start,
-                                  @RequestParam(required = false) Integer end) {
-    if (start == null || end == null) {
-      return rsList.toString();
+    private List<String> initRsList() {
+        List<String> tempList = new ArrayList<>();
+        tempList.add("第一条事件");
+        tempList.add("第二条事件");
+        tempList.add("第三条事件");
+        return tempList;
     }
-    return rsList.subList(start - 1, end).toString();
-  }
+
+    @GetMapping("/rs/{index}")
+    public String getOneRsEvent(@PathVariable int index) {
+        return rsList.get(index - 1);
+    }
+
+    @GetMapping("rs/list")
+    public String getRsEventByRange(@RequestParam(required = false) Integer start,
+                                    @RequestParam(required = false) Integer end) {
+        if (start == null || end == null) {
+            return rsList.toString();
+        }
+        return rsList.subList(start - 1, end).toString();
+    }
+
+    @PostMapping("rs/event")
+    public void addRsEvent(@RequestBody String rsEventString) {
+        rsList.add(rsEventString);
+    }
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -4,14 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class RsListApplicationTests {
 
     @Autowired
@@ -22,9 +26,11 @@ class RsListApplicationTests {
         mockMvc.perform(get("/rs/1"))
             .andExpect(status().isOk())
             .andExpect(content().string("第一条事件"));
+
         mockMvc.perform(get("/rs/2"))
             .andExpect(status().isOk())
             .andExpect(content().string("第二条事件"));
+
         mockMvc.perform(get("/rs/3"))
             .andExpect(status().isOk())
             .andExpect(content().string("第三条事件"));
@@ -44,4 +50,17 @@ class RsListApplicationTests {
             .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
     }
 
+    @Test
+    void should_add_one_rs_event() throws Exception {
+        mockMvc.perform(get("/rs/list"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
+
+        mockMvc.perform(post("/rs/event").content("第四条事件").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+
+        mockMvc.perform(get("/rs/list"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件, 第四条事件]"));
+    }
 }
