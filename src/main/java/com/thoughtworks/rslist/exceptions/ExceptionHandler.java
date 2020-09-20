324896888dab84113561edb1845f94be7f7c7a5a
crystalfn <969274162@qq.com>
2020-09-20 15:58:30 +0800

    [fangni] feat: add log

--- src/main/java/com/thoughtworks/rslist/exceptions/ExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/exceptions/ExceptionHandler.java
@@ -2,9 +2,13 @@ package com.thoughtworks.rslist.exceptions;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 @ControllerAdvice
 public class ExceptionHandler {
+    Logger logger = LogManager.getLogger(ExceptionHandler.class);
+
     @org.springframework.web.bind.annotation.ExceptionHandler({
         InvalidIndexException.class,
         InvalidRequestParamException.class,
@@ -14,6 +18,7 @@ public class ExceptionHandler {
     public ResponseEntity<CommentError> handleException(Exception error) {
         CommentError commentError = new CommentError();
         commentError.setErrorMessage(error.getMessage());
+        logger.error(error.getMessage());
         return ResponseEntity.badRequest().body(commentError);
     }
 }
